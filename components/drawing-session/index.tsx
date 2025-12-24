1b7548eb2a37e20e3f114e69875326d014ba7030
devnguy <hdn301@gmail.com>
2025-12-24 13:10:35 -0600

    fetch remaining pins contd

--- components/drawing-session/index.tsx
+++ components/drawing-session/index.tsx
@@ -10,60 +10,30 @@ import Link from "next/link";
 import useSWRInfinite, { SWRInfiniteKeyLoader } from "swr/infinite";
 import { fetcher } from "@/lib/api/pinterest/queries";
 import { ImageSourceResponse, Pin } from "@/app/types";
-import useSWR from "swr";
-import { auth } from "@/auth";
 
-export const testWithCursor = (boardId: string) => async () => {
-  const cursor =
-    "LT4xMzY0NDUwNjM3MDIxNzAxMjZ8MjUwfDEwOTYzOTQzMzg2MjkwMTQqR1FMKnw3YzllMjQxZjI4OGZhNWQ2YTQzY2U0OGU0YmJiYTA3MDQ0MzFiYjkzNDllMTVhMDI3MTYxNTNlYWEzMmJlMDg2fE5FV3w=";
-  const session = await auth();
-
-  if (!session) {
-    return {
-      bookmark: "",
-      items: [],
-    };
-  }
-  const response = await fetch(
-    `https://api.pinterest.com/v5/boards/${boardId}/pins?page_size=25${cursor}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${session?.access_token}`,
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    },
-  );
-  return await response.json();
-};
-
+// boardId: 136445132399848892
 const getKey =
   (boardId: string | undefined): SWRInfiniteKeyLoader =>
   (pageIndex: number, previousPageData: ImageSourceResponse<Pin>) => {
     console.log("getKey", boardId, pageIndex, previousPageData);
-    // if (boardId === undefined) {
-    //   return null;
-    // }
-    // reached the end
     // first page, we don't have `previousPageData`
     if (pageIndex === 0) {
       return `https://api.pinterest.com/v5/boards/${boardId}/pins?page_size=25`;
     }
-    // add the cursor to the API endpoint
 
+    // reached the end
     if (!previousPageData?.bookmark) {
       console.log("reached the end");
       return null;
     }
-    // `https://api.pinterest.com/v5/boards/${boardId}/pins?page_size=250${cursorSearchParam}`,
+
     return `https://api.pinterest.com/v5/boards/${boardId}/pins?page_size=25&bookmark=${previousPageData.bookmark}`;
   };
 
 export default function DrawingSession() {
   const { state, dispatch } = useDrawingSessionContext();
 
-  const { data, isLoading, error, size } = useSWRInfinite(
+  const { data, isLoading, error } = useSWRInfinite(
     getKey(state.boardId),
     fetcher,
     {
@@ -71,14 +41,9 @@ export default function DrawingSession() {
     },
   );
 
-  // useEffect(() => {
-  //   console.log({ data, isLoading, error, size });
-  // }, [data, isLoading, error, size]);
-
-  // const { data, isLoading, error } = useSWR(
-  //   "LT4xMzY0NDUwNjM3MDIxNzAxMjZ8MjUwfDEwOTYzOTQzMzg2MjkwMTQqR1FMKnw3YzllMjQxZjI4OGZhNWQ2YTQzY2U0OGU0YmJiYTA3MDQ0MzFiYjkzNDllMTVhMDI3MTYxNTNlYWEzMmJlMDg2fE5FV3w=",
-  //   testWithCursor("136445132399848892"),
-  // );
+  if (error) {
+    console.log(error);
+  }
 
   useEffect(() => {
     console.log({ data, isLoading, error });
